63faa7e2d35c24b926ae84ed4f8f5e06cad1764c
jaehunshin <jhshin0422@gmail.com>
2026-04-25 16:14:26 +0900

    feat: User 엔티티와 JPA Auditing 설정 추가

---
+++ src/main/java/org/sopt/domain/User.java
@@ -0,0 +1,36 @@
+package org.sopt.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String email;
+
+    protected User(){}
+
+    public User(String nickname, String email) {
+        this.nickname = nickname;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
