fe49dc1b998e6834309ee02cfe445084ba4a4079
jaehunshin <jhshin0422@gmail.com>
2026-04-25 15:50:39 +0900

    fix: PostRepository를 interface 로 변경

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -3,5 +3,5 @@ package org.sopt.repository;
 import org.sopt.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
