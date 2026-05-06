55e3e750545a0418dbc0356f0936fb726c91f743
jaehunshin <jhshin0422@gmail.com>
2026-05-06 14:00:23 +0900

    fix: Swagger HTTPS 요청 설정

---
+++ src/main/java/org/sopt/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package org.sopt.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "/")
+    }
+)
+public class SwaggerConfig {
+}
